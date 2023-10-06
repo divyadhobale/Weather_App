974f9cfa8047390b2b8546d8d24b4c871a02cd45
divyadhobale <divyadhobale0@gmail.com>
2023-10-06 19:36:39 +0530

    Work on Weather App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button } from 'bootstrap';
+//import { Button } from 'bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
@@ -12,7 +12,7 @@ function App() {
 
   const getWeatherDetails=(cityName)=>{
     if(!cityName) return
-  const apiURL="https://api.openweathermap.org/data/2.5/weather?q=" + cityName+ " &appid="+ apikey
+  const apiURL='https://api.openweathermap.org/data/2.5/weather?q=' + cityName+ " &appid="+ apikey
     axios.get(apiURL).then((res)=> {
       console.log("response",res)
       setData(res.data)
@@ -34,7 +34,7 @@ function App() {
 
   
   return (
-    <div className=" shadow rounded weatherBg">
+    <div className=" shadow rounded weatherBg" style={{backgroundImage:"url('white-cloud-blue-sky.jpg')"}}>
 
     <div className="col-md-12 text-center mt-5">
     
@@ -43,20 +43,21 @@ function App() {
       
         <h1 className="heading">Weather App</h1>
 
-        <div className="d-grid-gap-3 col-4 mt-4">
-          <input type="text"className="form_control"
+        <div className= "col-md-12 text-center mt-5">
+          <input type="text"className="form_control" justify-content='center'
           value={inputCity} 
           onChange={handleChangeInput}/> <br/>
-          <button className="btn btn-primary" type="button" onClick={handleSearch}>Search</button>
+          <button className="btn-btn-primary" type="button" onClick={handleSearch}>Search</button>
         </div>
          </div>
 
-            <h1 className="icon">🌞	</h1>
+            <h1 className="icon">🌞 </h1>{data?.weather?.[0].description} 
             <h1 className="wetherCity">{data?.name}</h1>
-            <h6 className="weatherTemp">{((data?.main?.temp)-273.15).toFixed(2)}°C</h6>
-            <h6 className="humidity">{data?.main?.humidity}</h6>
-           Sunrise :<h6 className="sunrise">{data?.sys?.sunrise}</h6>
-           Sunset: <h6 className="sunset">{data?.sys?.sunset}</h6>
+            <h6 className="weatherTemp">Temp:{((data?.main?.temp)-273.15).toFixed(2)}°C</h6>
+            <h6 className="humidity">Humidity : {data?.main?.humidity}</h6>
+             <h6 className="wind">Wind:{data?.wind?.speed}kph</h6>
+             {/* <h6 className="time">Time:{data?.timezone}</h6>   */}
+
 
           </div>
          
